560aa10e757d563c4dbf9ea9879968228372a05e
Huang Di <huangdi18s@ict.ac.cn>
2021-04-24 21:16:12 +0800

    bug

--- taskfinished.py
+++ taskfinished.py
@@ -16,12 +16,16 @@ with open('/lustre/S/huangdi/scripts/log.task_id',mode='r') as f:
     for l in f.readlines():
         history.append(l.replace('\n', ''))
 
+current = []
 f = open('/lustre/S/huangdi/scripts/log.task_id',mode='w')
 for item in output.split('\n')[1:]:
     if len(item.split()) < 10:
         continue
     task_id = item.split()[0]
-    if task_id not in history:
-        print(task_id)
+    current.append(task_id)
     f.write(task_id + '\n')
 f.close()
+
+for task_id in history:
+    if task_id not in current:
+        print(task_id)
