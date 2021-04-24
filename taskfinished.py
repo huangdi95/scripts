f79eb723a9906de6c06e2e50c7bd98542e1c42d8
Huang Di <huangdi18s@ict.ac.cn>
2021-04-24 21:44:25 +0800

    remove debug

--- taskfinished.py
+++ taskfinished.py
@@ -21,6 +21,8 @@ f = open('/lustre/S/huangdi/scripts/log.task_id',mode='w')
 for item in output.split('\n')[1:]:
     if len(item.split()) < 10:
         continue
+    if item.split()[3] == 'debug':
+        continue
     task_id = item.split()[0]
     current.append(task_id)
     f.write(task_id + '\n')
